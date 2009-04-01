27593816ef03bedc9c1b2a6daf4409717a293a5f
nfurmento <nfurmento@f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356>
2009-04-01 12:21:03 +0000

    Allow to specify parameters when calling application
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/open-mx/branches/irq@2562 f1ba3bf5-cb5c-402b-92a9-7c6bdc83a356

--- tests/omx_spam.c
+++ tests/omx_spam.c
@@ -26,19 +26,36 @@
 #include "omx_config.h"
 #include "omx_lib.h"
 
+#define BID 0
+#define DID 0
+#define NB  1000000
+
+static void
+usage(int argc, char *argv[])
+{
+  fprintf(stderr, "%s [options]\n", argv[0]);
+  fprintf(stderr, "Common options:\n");
+  fprintf(stderr, " -b <n>\tchange board id [%d]\n", BID);
+  fprintf(stderr, " -d <n>\tchange destination id [%d]\n", DID);
+  fprintf(stderr, " -n <n>\tchange number of sent messages [%d]\n", NB);
+}
+
 static inline int
-do_spam(int fd, int index)
+do_spam(int fd, int peer_index, int board_index, int nb)
 {
   struct omx_cmd_send_spam spam;
-  spam.peer_index = index;
-  spam.board_index = 0;
-  spam.nb = 1000000;
+  spam.peer_index = peer_index;
+  spam.board_index = board_index;
+  spam.nb = nb;
   return ioctl(fd, OMX_CMD_SEND_SPAM, &spam);
 }
 
 int main(int argc, char *argv[])
 {
-  int fd;
+  int fd, c;
+  int bid = BID;
+  int did = DID;
+  int nb = NB;
 
   fd = open("/dev/" OMX_MAIN_DEVICE_NAME, O_RDWR);
   if (fd < 0) {
@@ -46,7 +63,26 @@ int main(int argc, char *argv[])
     return -1;
   }
 
-  do_spam(fd, 1);
+  while ((c = getopt(argc, argv, "d:b:n:h")) != -1)
+    switch (c) {
+    case 'b':
+      bid = atoi(optarg);
+      break;
+    case 'd':
+      did = atoi(optarg);
+      break;
+    case 'n':
+      nb = atoi(optarg);
+      break;
+    default:
+      fprintf(stderr, "Unknown option -%c\n", c);
+    case 'h':
+      usage(argc, argv);
+      exit(-1);
+      break;
+    }
+
+  do_spam(fd, did, bid, nb);
   return 0;
 }
 
